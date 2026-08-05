f1636d70f76b9aca97380ca8f0a550731e70e828
Alvaro Segura <alvaro.segura@gmail.com>
2026-08-05 19:11:21 +0200

    Extended test and fixed for gcc

--- tests.cpp
+++ tests.cpp
@@ -19,7 +19,7 @@ inline Vec3 randomVec3(float k)
 
 ASL_TEST(fiPlaneXY)
 {
-	Array<Vec3> points;
+	Array<Vec3>  points;
 	Array<float> planeXY = { 0.1f, -0.05f, 10.3f };
 	for (int i = 0; i < 2000; i++)
 	{
@@ -50,7 +50,7 @@ ASL_TEST(fitSphere)
 {
 	Array<Vec3> points;
 	Vec3        center = randomVec3(100);
-	float radius = asl::random(50.f, 100.f);
+	float       radius = asl::random(50.f, 100.f);
 	for (int i = 0; i < 2000; i++)
 	{
 		Vec3 p = center + randomVec3(1.0f).normalized() * radius;
@@ -68,20 +68,27 @@ ASL_TEST(fitSphere)
 	ASL_EXPECT_NEAR(sphereR.w, radius, 0.01f);
 }
 
-
 ASL_TEST(fitLine)
 {
 	Array<Vec3> points;
 	asl::Vec3   origin = randomVec3(100);
 	asl::Vec3   dir = randomVec3(1.0f).normalized();
-	
+
 	for (int i = 0; i < 2000; i++)
 	{
-		points << origin + dir * random(-500.f, 500.f) + randomVec3(1.0f);
+		points << origin + dir * asl::random(-500.f, 500.f) + randomVec3(1.0f);
 	}
 
 	auto line = fitLine(points);
-	//ASL_EXPECT_NEAR(line.first, origin, 1.0f);
+
 	ASL_EXPECT(distancePointLine(origin, line.first, line.second), <, 1.0f);
 	ASL_ASSERT(line.second.angle(dir) < deg2rad(1.f) || line.second.angle(dir) > deg2rad(179.f));
+
+	for (int i = 0; i < 20; i++)
+		points << randomVec3(1000.f);
+
+	auto lineR = fitLineR(points, 2.0f);
+
+	ASL_EXPECT(distancePointLine(origin, lineR.first, lineR.second), <, 1.0f);
+	ASL_ASSERT(lineR.second.angle(dir) < deg2rad(1.f) || lineR.second.angle(dir) > deg2rad(179.f));
 }
